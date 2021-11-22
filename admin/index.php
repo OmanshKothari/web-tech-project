3191ca873ccafd94042d6374f151f11807c38d0d
Omansh <kothariomansh@gmail.com>
2021-11-22 18:39:35 +0530

    Started Working on "Renewed Admin Panel".

--- admin/index.php
+++ admin/index.php
@@ -5,35 +5,27 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Adminsitration Page</title>
-    <link rel="stylesheet" href="../css/style.css" />
-    <link rel="stylesheet" href="../css/admin.css" />
+    <title>Administration Panel</title>
+    <link rel="stylesheet" href="../css/admin.css">
 </head>
 
 <body>
-    <div class="menu  text-center">
-        <div class="container">
-            <div class="header">
-                <nav class="navbar">
-                    <a href="#home">home</a>
-                    <a href="#about">about</a>
-                    <a href="#menu">menu</a>
-                    <a href="#products">products</a>
-                    <a href="#review">review</a>
-                    <a href="#contact">contact us</a>
-                    <a href="#blogs">blogs</a>
-                </nav>
-            </div>
+    <!-- Menu Section Starts -->
+    <div class="menu">
+        <div class="wrapper">
+            Menu Goes Here
         </div>
     </div>
-    <div class="main-content text-center">
-        <div class="container">
-            <h1>Main content Goes Here</h1>
+    <!-- Main Content Section Starts -->
+    <div class="main-content">
+        <div class="wrapper">
+            Main Content Goes Here
         </div>
     </div>
-    <div class="footer text-center">
-        <div class="container">
-            <h1>Footer Goes Here</h1>
+    <!-- Footer Goes Here -->
+    <div class="footer">
+        <div class="wrapper">
+            Footer Goes Here
         </div>
     </div>
 </body>
