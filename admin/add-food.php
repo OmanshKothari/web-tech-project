2fcd9107446c2c0f8bbe9f300af8d423950554dc
Omansh <kothariomansh@gmail.com>
2021-11-24 09:52:26 +0530

    "Added the functionality to add new food item.[manage-food.php and add-food.php]"

---
+++ admin/add-food.php
@@ -0,0 +1,128 @@
+<?php
+include('partials/header.php')
+?>
+<!-- Main Content Section Starts -->
+<div class="main-content  mc-bg wrapper">
+    <div class="box-container">
+        <h1>Add Food</h1>
+
+        <div class="text-center">
+            <?php
+            if (isset($_SESSION['add_food'])) {
+                echo "<h3>" . $_SESSION['add_food'] . "</h3>"; // Displaying Session Message
+                unset($_SESSION['add_food']);
+                // Removes the variable from the session variables so that message is displayed only once
+            }
+            ?>
+        </div>
+
+        <form id="#manage-form" action="" method="POST">
+            <table class="text-center">
+                <tr>
+                    <td>Title : </td>
+                    <td>
+                        <input type="text" name="title" placeholder="Enter Food Title ">
+                    </td>
+                </tr>
+                <tr>
+                <tr>
+                    <td>Description : </td>
+                    <td>
+                        <textarea  rows="10" cols="60" name="description" placeholder="Enter Food Description "></textarea>
+                    </td>
+                </tr>
+                <tr>
+                <tr>
+                    <td>Price : </td>
+                    <td>
+                        <input type="number" name="price" placeholder="Enter Food Price" step="0.01">
+                    </td>
+                </tr>
+                <tr>
+                    <td>Image name : </td>
+                    <td>
+                        <input type="text" name="image_name" placeholder="Enter Food Image Name">
+                    </td>
+                </tr>
+                <tr>
+                </tr>
+                <tr>
+                    <td>Category ID : </td>
+                    <td>
+                        <input type="number" name="category_id" placeholder="Enter Food Category ID">
+                    </td>
+                </tr>
+                <tr>
+                    <td>Featured : </td>
+                    <td>
+                        <label for="featured-yes">YES : </label><input type="checkbox" name="featured" value="YES">
+                        <label for="featured-no">NO : </label><input type="checkbox" name="featured" value="NO">
+                    </td>
+                </tr>
+                <tr>
+                    <td>Active : </td>
+                    <td>
+                        <label for="active-yes">YES : </label><input type="checkbox" name="active" value="YES">
+                        <label for="active-no">NO : </label><input type="checkbox" name="active" value="NO">
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2">
+                        <input type="submit" name="submit" value="Add Category" class="btn-primary">
+                    </td>
+                </tr>
+            </table>
+        </form>
+    </div>
+</div>
+<?php
+include('partials/footer.php')
+?>
+
+<?php
+// Process the value from Form and save it in the Database
+
+// Check whether the button is clicked or not
+
+if (isset($_POST['submit'])) {
+    // Button Clicked
+
+    $title = $_POST['title'];
+    $description = $_POST['description'];
+    $price = $_POST['price'];
+    $image_name = $_POST['image_name'];
+    $category_id = $_POST['category_id'];
+    $featured = $_POST['featured'];
+    $active = $_POST['active'];
+
+    // SQL query to save Data in Database
+
+    $sql = "INSERT INTO tb_food SET
+            title = '$title',
+            description = '$description',
+            price = '$price',
+            image_name = '$image_name',
+            category_id = '$category_id',
+            featured = '$featured',
+            active = '$active'
+        ";
+
+    // Executing query to save Data
+
+    // Declaring variable to check whether it the query executes successfully
+
+    $res = mysqli_query($conn, $sql);
+
+    if ($res) {
+        // Create a session variable to display the status of query execution
+        $_SESSION['add_food'] = "Food Added Successfully!";
+        // REDIRECT PAGE to manage-admin
+        header('location:' . SITEURL . 'admin/manage-food.php');
+    } else {
+        // Create a session variable to display the status of query execution
+        $_SESSION['add_category'] = "Failed to Add Food!";
+        // REDIRECT PAGE to same page
+        header('location:' . SITEURL . 'admin/add-food.php');
+    }
+}
+?>
--- admin/manage-food.php
+++ admin/manage-food.php
@@ -1,60 +1,93 @@
-<?php 
-    include('partials/header.php')
+<?php
+include('partials/header.php')
 ?>
 <!-- Main Content Section Starts -->
 <div class="main-content  mc-bg wrapper">
     <div class="box-container">
-        <h2>Manage Food</h2>   
-        
-        <!-- Button to add admin -->
-        <div class='btn-container'><a href="#" class="btn-primary">Add Food</a></div>
+        <h2>Manage Food</h2>
 
+        <!-- Button to add Food -->
+        <div class='btn-container'><a href="add-food.php" class="btn-primary">Add Food</a></div>
+        <br>
+        <!-- Displaying Session message for adding food -->
+        <div class="text-center">
+            <?php
+            if (isset($_SESSION['add_food'])) {
+                echo "<h3>" . $_SESSION['add_food'] . "</h3>"; // Displaying Session Message
+                unset($_SESSION['add_food']);
+                // Removes the variable from the session variables so that message is displayed only once
+            }
+            ?>
+        </div>
+        <br>
+        <br>
         <table>
             <tr>
                 <th>S.No.</th>
-                <th>Full Name</th>
-                <th>Username</th>
+                <th>Title</th>
+                <th>Description</th>
+                <th>Price</th>
+                <th>Image_name</th>
+                <th>Category id</th>
+                <th>Featured</th>
+                <th>Active</th>
                 <th>Actions</th> <!-- This will have buttons like update, remove etc. -->
             </tr>
-            <tr>
-                <td>1. </td>
-                <td>Omansh Kothari</td>
-                <td>omkothari</td>
-                <td>
-                    <a href="#" class="btn-secondary">Update Admin</a>
-                    <a href="#" class="btn-danger">Delete Admin</a>
-                </td>
-            </tr>
-            <tr>
-                <td>1. </td>
-                <td>Omansh Kothari</td>
-                <td>omkothari</td>
-                <td>
-                    <a href="#" class="btn-secondary">Update Admin</a>
-                    <a href="#" class="btn-danger">Delete Admin</a>
-                </td>
-            </tr>
-            <tr>
-                <td>1. </td>
-                <td>Omansh Kothari</td>
-                <td>omkothari</td>
-                <td>
-                    <a href="#" class="btn-secondary">Update Admin</a>
-                    <a href="#" class="btn-danger">Delete Admin</a>
-                </td>
-            </tr>
-            <tr>
-                <td>1. </td>
-                <td>Omansh Kothari</td>
-                <td>omkothari</td>
-                <td>
-                    <a href="#" class="btn-secondary">Update Admin</a>
-                    <a href="#" class="btn-danger">Delete Admin</a>
-                </td>
-            </tr>
+            <?php
+            // SQL query to get all the rows from database
+            $sql = "SELECT * FROM tb_food";
+            // Executing the query
+            $res = mysqli_query($conn, $sql);
+            // Counting the number of rows in database
+            $count = mysqli_num_rows($res);
+
+            // Create a Serial number variable
+            $sn = 1;
+
+            if ($count > 0) {
+                // Meaning the database is not empty
+                while ($rows = mysqli_fetch_assoc($res)) {
+                    // using while loop to get records from database
+                    // loop will run as long as we have rows in database
+
+                    // Getting the data
+
+                    $id = $rows['id'];
+                    $title = $rows['title'];
+                    $description = $rows['description'];
+                    $price = $rows['price'];
+                    $image_name = $rows['image_name'];
+                    $category_id = $rows['category_id'];
+                    $featured = $rows['featured'];
+                    $active = $rows['active'];
+
+
+                    // Display value in table
+            ?>
+                    <tr>
+                        <td><?php echo $sn; ?></td>
+                        <td><?php echo $title; ?></td>
+                        <td><?php echo $description; ?></td>
+                        <td><?php echo $price; ?></td>
+                        <td><?php echo $image_name; ?></td>
+                        <td><?php echo $category_id; ?></td>
+                        <td><?php echo $featured; ?></td>
+                        <td><?php echo $active; ?></td>
+                        <td>
+                            <a href="#" class="btn-secondary">Update Category</a>
+                            <a href="#" class="btn-danger">Delete Category</a>
+                        </td>
+                    </tr>
+            <?php
+                    $sn++;
+                }
+            } else {
+                // Database is empty
+            }
+            ?>
         </table>
     </div>
 </div>
-<?php 
-    include('partials/footer.php')
+<?php
+include('partials/footer.php')
 ?>
